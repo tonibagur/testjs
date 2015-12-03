c983b81f63819b8484a198414abf68a020f2bc4d
MeritxellMa <m.marblu@gmail.com>
2015-12-03 10:25:36 +0100

    karma coverage added

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,12 +5,26 @@ module.exports = function(config){
 
     files : [
       'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/sinonjs/sinon.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
     ],
+
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/js/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     autoWatch : true,
 
@@ -21,7 +35,8 @@ module.exports = function(config){
     plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
-            'karma-jasmine'
+            'karma-jasmine',
+	    'karma-coverage'
             ],
 
     junitReporter : {
